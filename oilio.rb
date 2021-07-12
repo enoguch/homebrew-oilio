737be356e85d038619d0f015a9ad76ef84c70e22
sakamoto <pinponsumairu@gmail.com>
2021-07-12 18:17:58 +0900

    update oilio.rb

---
+++ oilio.rb
@@ -0,0 +1,12 @@
+HOMEBREW_OILIO_VERSION="1.0.0"
+
+class Oilio < Formula
+  desc "Eliminating duplicated lines"
+  homepage "https://github.com/enoguch/oilio"
+  url "https://github.com/enoguch/oilio/releases/download/v#{HOMEBREW_OILIO_VERSION}/oilio-#{HOMEBREW_OILIO_VERSION}_darwin_amd64.tar.gz"
+  version HOMEBREW_OILIO_VERSION
+
+  def install
+    bin.install "oilio"
+  end
+end

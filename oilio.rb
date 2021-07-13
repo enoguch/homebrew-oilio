ec1f224f49a68230e71379db02ee94bed441faf1
sakamoto <pinponsumairu@gmail.com>
2021-07-13 12:00:16 +0900

    update

--- oilio.rb
+++ oilio.rb
@@ -3,8 +3,9 @@ HOMEBREW_OILIO_VERSION="1.0.0"
 class Oilio < Formula
   desc "Eliminating duplicated lines"
   homepage "https://github.com/enoguch/oilio"
-  url "https://github.com/enoguch/oilio/releases/download/#{HOMEBREW_OILIO_VERSION}/oilio-#{HOMEBREW_OILIO_VERSION}_linux_amd64.tar.gz"
+  url "https://github.com/enoguch/oilio/releases/download/#{HOMEBREW_OILIO_VERSION}/oilio-#{HOMEBREW_OILIO_VERSION}_darwin_amd64.tar.gz"
   version HOMEBREW_OILIO_VERSION
+  sha256 "611a05ce8637f904f8b549ed49337dd317523dc08d65f842f65e59865989413e"
 
   def install
     bin.install "oilio"
